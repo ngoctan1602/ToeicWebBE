81480be189e19ab4d9fa6492a751726676722968
ASUS <nguyenthaingoctan16@gmail.com>
2024-04-01 13:45:49 +0700

    update create question

--- src/main/java/com/tantan/ToeicWeb/ToeicWebApplication.java
+++ src/main/java/com/tantan/ToeicWeb/ToeicWebApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ToeicWebApplication {
-
 	public static void main(String[] args) {
 		SpringApplication. run(ToeicWebApplication.class, args);
 	}
-
 }
--- src/main/java/com/tantan/ToeicWeb/request/ParagraphRequest.java
+++ src/main/java/com/tantan/ToeicWeb/request/ParagraphRequest.java
@@ -1,14 +1,23 @@
 package com.tantan.ToeicWeb.request;
 
+import jakarta.annotation.Nullable;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.ArrayList;
 
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
 public class ParagraphRequest {
-    private String content;
-    private String img;
     private Long idPart;
+    private String content;
+    private ArrayList<QuestionParagraphRequest> questionParagraphRequests;
+    @Nullable
+    private MultipartFile image;
+
+    @Nullable
+    private MultipartFile audio;
 }
